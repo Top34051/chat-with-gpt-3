de057349e70dfa6b53d551bbddbdc6204b3cc5d0
Jirayu Burapacheep <top34051@gmail.com>
2021-11-14 01:54:16 -0600

    update survey id and response count

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import openai
 import os
+import random
 
 
 st.set_page_config(layout='wide', page_title='GPT-3 chatbot')
 openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+response_limit = [3, 5]
 
 
 def get_text():
@@ -40,6 +43,8 @@ def main():
     st.session_state['prompt'] = 'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n'
   if 'response_count' not in st.session_state:
     st.session_state['response_count'] = 0
+  if 'survey_id' not in st.session_state:
+    st.session_state['survey_id'] = random.randint(100000, 999999)
   
   user_input = get_text()
   if user_input != '':
@@ -47,26 +52,30 @@ def main():
     # add user input to prompt
     st.session_state['prompt'] = st.session_state['prompt'] + 'Human: ' + user_input + '\n' + 'AI:'
     
-    # get response from bot
-    response = get_response().choices[0].text
-    st.text_area("Bot:", value=response, height=200, max_chars=None, key=None)
-    
-    # add response to prompt
-    st.session_state['prompt'] = st.session_state['prompt'] + response + '\n'
-    st.session_state['response_count'] += 1
+    if st.session_state['response_count'] < response_limit[1]:
 
-    st.write('Response count:', st.session_state['response_count'])
+      # get response from bot
+      response = get_response().choices[0].text
+      st.text_area("Bot:", value=response, height=200, max_chars=None, key=None)
+      
+      # add response to prompt
+      st.session_state['prompt'] = st.session_state['prompt'] + response + '\n'
+      st.session_state['response_count'] += 1
+
+    response_count_message = f"Response count: {st.session_state['response_count']}"
+    if st.session_state['response_count'] < response_limit[0]:
+      st.success(response_count_message)
+    elif st.session_state['response_count'] < response_limit[1]:
+      extra_count = response_limit[1] - st.session_state['response_count']
+      st.warning(response_count_message + f'. The survey has ended, but you can ask {extra_count} more questions.')
+    else:
+      st.error(response_count_message + '. The survey has ended!')
+
 
   # submit survey
-  with st.form(key='submit_form'):
-    survey_id = st.text_input('Your survey identification number')
-    clicked = st.form_submit_button('Complete survey')
-    if clicked:
-      if st.session_state['response_count'] >= 20:
-        st.success('Survey completed! Please close this window and complete a post-survey.')
-        # save prompt
-      else:
-        st.error('Please make more conversation with the chatbot') 
+  if st.session_state['response_count'] >= response_limit[0]:
+    survey_id = st.session_state['survey_id']
+    st.success(f'**Survey completed!**\n\n{survey_id} is your identification number. Please close this window, return to the post-survey and put this number in.')
 
 
 if __name__ == '__main__':
